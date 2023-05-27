4d7bf67d47e86f7ff5cb058e6914c1ae26c5c46c
Chetan Desai <chetandesai2488@gmail.com>
2023-05-27 10:34:48 +0530

    Add map view controller

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello Word!")
+        let mapView = MapViewController()
+        print(mapView)
+        
     }
 
 
